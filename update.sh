b531997074deb9aadb3ab735513a7046ad2d0d9c
Ante de Baas <5467398+antedebaas@users.noreply.github.com>
2023-08-31 19:42:09 +0200

    Enable systemd timer (#1)

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Get latest version from git"
+git pull
+
+echo "Install composer dependencies"
+composer install
+
+echo "clear cache"
+php bin/console cache:clear
+
+echo "run migrations"
+php bin/console doctrine:migrations:migrate --no-interaction --query-time
+
+echo "install assets"
+php bin/console assets:install public
+
+echo "warmup cache"
+php bin/console cache:warmup
