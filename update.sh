8b6a4d8cad45190b8801ce9fc83e074eef536427
Ante de Baas <ante@debaas.net>
2023-08-31 19:37:43 +0200

    update

--- update.sh
+++ update.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "Get latest version from git"
 git pull
+
 echo "Install composer dependencies"
 composer install
 
-echo "clear cache and run migrations"
+echo "clear cache"
 php bin/console cache:clear
+
+echo "run migrations"
 php bin/console doctrine:migrations:migrate --no-interaction --query-time
 
-echo "warmup cache and install assets"
+echo "install assets"
 php bin/console assets:install public
+
+echo "warmup cache"
 php bin/console cache:warmup
