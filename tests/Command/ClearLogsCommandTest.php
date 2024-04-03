61e1b823f8b27dba30a1cdf3c7f8f5c7f88a5b4e
Ante de Baas <ante@debaas.net>
2024-04-03 23:46:56 +0200

    add testing

---
+++ tests/Command/ClearLogsCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Tests\Command;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ClearLogsCommandTest extends KernelTestCase
+{
+    public function testExecute(): void
+    {
+        self::bootKernel();
+        $application = new Application(self::$kernel);
+
+        $command = $application->find('app:clearlogs');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $commandTester->assertCommandIsSuccessful();
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('[OK] Logs have been cleared', $output);
+    }
+}
