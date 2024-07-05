6ea1de6b4198c0405b2c942892ff0e965432c83d
Ante de Baas <ante@debaas.net>
2024-07-05 22:30:44 +0200

    getreportsfrommailbox output changed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env.test');
 }
 
 if ($_SERVER['APP_DEBUG']) {
