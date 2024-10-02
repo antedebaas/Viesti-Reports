04e108041b724b6177e2ff9c1eb9a4ade820a0c7
Ante de Baas <ante@debaas.net>
2024-10-02 14:51:54 +0200

    add pushover support

--- src/Command/GetReportsFromMailboxCommand.php
+++ src/Command/GetReportsFromMailboxCommand.php
@@ -94,6 +94,16 @@ class GetReportsFromMailboxCommand extends Command
                     $results['secondary']->setSuccess(true, 'Secondary mailbox is disabled.', array('count' => 0, 'reports' => array()));
                 }
 
+                if(!empty($this->params->get('app.pushover_api_key')) && !empty($this->params->get('app.pushover_user_key')))
+                {
+                    $count = $results['primary']->getDetails()["count"] + $results['secondary']->getDetails()["count"];
+                    $application = new Application($this->params->get('app.pushover_api_key'));
+                    $recipient = new Recipient($this->params->get('app.pushover_user_key'));
+                    $message = new Message($count.' new emails have been processed by viesti reports', 'New reports processed.');
+                    $notification = new Notification($application, $recipient, $message);
+                    $notification->push();
+                }
+
                 $log = new Logs();
                 $log->setTime(new \DateTime());
                 $log->setSuccess($results['primary']->getSuccess());
