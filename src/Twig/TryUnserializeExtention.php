3f9e1e6869dd3f0742b43f49bfcd92e1f19cc974
Ante de Baas <ante@debaas.net>
2024-10-03 21:23:12 +0200

    fix try_unserialize

--- src/Twig/TryUnserializeExtention.php
+++ src/Twig/TryUnserializeExtention.php
@@ -17,15 +17,9 @@ class TryUnserializeExtention extends AbstractExtension
 
     public function try_unserialize($data): array
     {
-        if(is_string($data)) {
-            try {
-                $data = unserialize($data);
-            } catch (\Exception $e) {
-                $data = array($data);
-            }
-        } elseif (is_null($data)) {
-            $data = array();
-        } else {
+        try {
+            $data = unserialize($data);
+        } catch (\Exception $e) {
             $data = array($data);
         }
         return $data;
