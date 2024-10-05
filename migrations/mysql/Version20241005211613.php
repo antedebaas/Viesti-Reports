1211f5c97a2a29b20c4147b2d1083fbd996d4f66
Ante de Baas <ante@debaas.net>
2024-10-05 23:21:47 +0200

    truncate first

--- migrations/mysql/Version20241005211613.php
+++ migrations/mysql/Version20241005211613.php
@@ -20,14 +20,14 @@ final class Version20241005211613 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE logs ADD state INT NOT NULL, ADD mailcount INT NOT NULL, DROP success, CHANGE message message LONGTEXT NOT NULL, CHANGE details details LONGTEXT NOT NULL');
         $this->addSql('TRUNCATE TABLE logs');
+        $this->addSql('ALTER TABLE logs ADD state INT NOT NULL, ADD mailcount INT NOT NULL, DROP success, CHANGE message message LONGTEXT NOT NULL, CHANGE details details LONGTEXT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE logs ADD success TINYINT(1) NOT NULL, DROP state, DROP mailcount, CHANGE message message LONGTEXT DEFAULT NULL, CHANGE details details LONGTEXT DEFAULT NULL');
         $this->addSql('TRUNCATE TABLE logs');
+        $this->addSql('ALTER TABLE logs ADD success TINYINT(1) NOT NULL, DROP state, DROP mailcount, CHANGE message message LONGTEXT DEFAULT NULL, CHANGE details details LONGTEXT DEFAULT NULL');
     }
 }
--- migrations/postgresql/Version20241005211400.php
+++ migrations/postgresql/Version20241005211400.php
@@ -21,22 +21,22 @@ final class Version20241005211400 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE logs ADD state INT NOT NULL');
+        $this->addSql('TRUNCATE logs');
         $this->addSql('ALTER TABLE logs ADD mailcount INT NOT NULL');
         $this->addSql('ALTER TABLE logs DROP success');
         $this->addSql('ALTER TABLE logs ALTER message SET NOT NULL');
         $this->addSql('ALTER TABLE logs ALTER details SET NOT NULL');
-        $this->addSql('TRUNCATE logs');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('TRUNCATE logs');
         $this->addSql('ALTER TABLE logs ADD success BOOLEAN NOT NULL');
         $this->addSql('ALTER TABLE logs DROP state');
         $this->addSql('ALTER TABLE logs DROP mailcount');
         $this->addSql('ALTER TABLE logs ALTER message DROP NOT NULL');
         $this->addSql('ALTER TABLE logs ALTER details DROP NOT NULL');
-        $this->addSql('TRUNCATE logs');
     }
 }
